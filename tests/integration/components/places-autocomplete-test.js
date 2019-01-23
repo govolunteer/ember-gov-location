2570ecc1414b8f4bf03beb2d556db0b4e7c1707b
Marc Altmann <marcaltmann@posteo.de>
2019-01-23 14:05:09 +0100

    Improve tests

--- tests/integration/components/places-autocomplete-test.js
+++ tests/integration/components/places-autocomplete-test.js
@@ -7,23 +7,29 @@ module('Integration | Component | places-autocomplete', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    assert.expect(3);
-
+    assert.expect(2);
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
-
     this.set('initialValue', 'Berlin');
-    this.set('placeholder', 'Enter location');
-
-    await render(hbs`{{places-autocomplete initialValue=initialValue placeholder=placeholder}}`);
+    await render(hbs`{{places-autocomplete initialValue=initialValue}}`);
 
     //const wrapperEl = this.element.querySelector('div');
     const inputEl = this.element.querySelector('input');
-
     assert.equal(inputEl.value, 'Berlin', 'initial input value is set');
-    assert.equal(inputEl.getAttribute('placeholder'), 'Enter location', 'placeholder is set');
 
     this.set('initialValue', 'Bamberg');
     assert.equal(inputEl.value, 'Berlin', 'input value does not change when initialValue changes');
+  });
+
+  test('it accepts placeholder option', async function(assert) {
+    await render(hbs`{{places-autocomplete placeholder="Enter location"}}`);
+    const actual = this.element.querySelector('input').getAttribute('placeholder');
+    const expected = 'Enter location';
+    assert.equal(actual,expected);
+  });
+
+  test('it renders inputClass attribute', async function(assert) {
+    await render(hbs`{{places-autocomplete inputClass="dummy-class"}}`);
+    assert.ok(this.element.querySelector('input').classList.contains('dummy-class'));
   });
 });
